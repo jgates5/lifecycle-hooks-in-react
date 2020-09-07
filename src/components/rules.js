396fffccc00973883754f4f8922a9dea3f96fc2d
jeffwgates <jeff.gates0507@gmail.com>
2020-09-07 13:59:31 -0600

    first intial commit

---
+++ src/components/rules.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Rules extends Component {
+
+   render() {
+      return (
+         <h1>
+           rules..
+         </h1>
+      )
+   }
+}
